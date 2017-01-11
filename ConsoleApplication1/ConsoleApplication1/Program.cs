1944d275f8c93fdd2b1d43512bc6191a401ddf86
arunsampathkalyan <arun.sampathkalyan@gmail.com>
2017-01-11 12:42:26 +0530

    Collaborator added in Github

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,8 +11,26 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("Git source control test application - test check in with Kesavan Credentials!");
-            Console.WriteLine("Git source control test application- test with Arun credentails!");
+            Console.WriteLine("Git source control test application- test with Arun credentails - added as a collaborator!");
             Console.Read();
+
+            /*
+             http://stackoverflow.com/questions/14969281/git-how-to-give-people-push-access-that-needs-to-be-merged-in-by-organizers
+
+            Git how to give people push access that needs to be merged in by organizers?
+
+            Ask for the username of the person you're adding as a collaborator. If they don't have one, they can sign up for GitHub.
+In the top right corner of any page, click your username.
+On your profile page, click the Repositories tab, then click the name of your repository.
+In your repository's right sidebar, click Settings.
+Click the "Collaborators" tab.
+Start typing the collaborator's username.
+Select the user from the drop-down menu.
+Click Add.
+
+
+
+             */
         }
     }
 }
